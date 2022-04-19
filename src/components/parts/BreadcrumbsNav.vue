dd5bee29392c2aa9a989382dbce7903fc12edb42
kochenov <kochenof@gmail.com>
2022-04-19 09:23:26 +0300

    Компоненты заготовки

---
+++ src/components/parts/BreadcrumbsNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "BreadcrumbsNav",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/parts/PromoLink.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "PromoLink",
+};
+</script>
+
+<style lang="scss" scoped></style>
