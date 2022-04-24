c0154c1f84edc53c306c9a2f4898b69b72a1e81e
kochenov <kochenof@gmail.com>
2022-04-24 15:41:42 +0300

    Добавлены пустые компоненты для табов расчёта гряд

---
+++ src/components/parts/seeding/HistorySeeding.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>History</div>
+</template>
+
+<script>
+export default { name: "HistorySeeding" };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/parts/seeding/UserSeeding.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>User</div>
+</template>
+
+<script>
+export default {
+  name: "UserSeeding",
+};
+</script>
+
+<style lang="scss" scoped></style>
