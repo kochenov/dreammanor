136c12ae8d838cfa3c1662035450d360269f14c7
kochenov <kochenof@gmail.com>
2022-04-24 15:42:24 +0300

    Добавлен компонент формы

---
+++ src/components/ui/form/FormGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <form @submit.prevent="" action="" method="GET">
+    <slot />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormGrid",
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  gap: 30px;
+}
+</style>
