4a8f0ede303c21812c04de07b2b23fc5d68a1728
kochenov <kochenof@gmail.com>
2022-04-24 15:40:07 +0300

    Добавлен компонент формы SELECT

---
+++ src/components/ui/form/SelectForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <LabelForm :idLabel="idLabel" :name="name">
+    <select :id="idLabel" :value="modelValue" @change="changeOption">
+      <slot />
+      <option v-for="option in options" :key="option.id" :value="option.id">
+        {{ option.name }}
+      </option>
+    </select>
+  </LabelForm>
+</template>
+
+<script>
+import LabelForm from "./LabelForm.vue";
+export default {
+  name: "SelectForm",
+  props: {
+    idLabel: {
+      type: String,
+      default: null,
+    },
+    name: {
+      type: String,
+      default: null,
+    },
+    modelValue: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {};
+  },
+  components: { LabelForm },
+  methods: {
+    changeOption(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@include grup-input();
+</style>
---
+++ src/components/ui/form/SelectOptionForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <option><slot /></option>
+</template>
+
+<script>
+export default {
+  name: "SelectOptionForm",
+};
+</script>
+
+<style lang="scss" scoped></style>
