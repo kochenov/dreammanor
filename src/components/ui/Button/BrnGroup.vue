40b279f34d798578c28d8c7b902b633a072c0b10
kochenov <kochenof@gmail.com>
2022-04-24 15:45:26 +0300

    Добавлены компоненты кнопок

---
+++ src/components/ui/Button/BrnGroup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="btn-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.btn-group {
+  margin: 10px 0;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+</style>
